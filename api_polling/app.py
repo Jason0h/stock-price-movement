09c2d5c631e9ce1d81efd3d635cf6c5a130b737a
jasonoh <jasno130@gmail.com>
2025-06-04 16:31:45 -0400

    first lambda completed

--- api_polling/app.py
+++ api_polling/app.py
@@ -1,12 +1,26 @@
 import json
+import yfinance as yf
+from datetime import datetime as dt
+from datetime import timedelta as td
+import pandas as pd
+
+STOCK = "NVDA"
 
 def lambda_handler(event, context): 
-    for record in event['Records']:
-        body = record['body']
-        print("SQS Message Body:", body)
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-        }),
-    }
+    utc_time_str = json.loads(event["Records"][0]["body"])["message"]
+    print("utc_time_str:", utc_time_str)
+    stock_price = retrieve_price(utc_time_str)
+    print("stock_price:", stock_price)
+    
+def retrieve_price(utc_time_str):
+    # convert to est timezone and pandas timestamp format. then create +-30s offsets (for snapping to nearest full minute)
+    utc_time_dt = dt.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%SZ")
+    utc_time_ts = pd.Timestamp(utc_time_dt, tz="UTC")
+    est_time_ts = utc_time_ts.tz_convert("America/New_York")
+    start_time = est_time_ts - td(seconds=30)
+    end_time = est_time_ts + td(seconds=30)
+    # poll yahoo finance api and retrieve stock price at between start_time and end_time 
+    stock = yf.Ticker(STOCK)
+    history = stock.history(period="3d", interval="1m")
+    price = history.loc[(history.index >= start_time) & (history.index <= end_time)]["Close"].iloc[0]
+    return price
---
+++ tests/unit/test_api_polling.py
@@ -0,0 +1,8 @@
+import pytest
+
+def test_retrieve_price():
+    UTC_TIME_STR_1 = "2025-06-04T19:41:20Z" # 3:41:20 PM EST
+    PRICE_1 = 141.70 # rough price at 3:41:20 PM EST
+    UTC_TIME_STR_2 = 
+    PRICE_2 = 
+    UTC_TIME_STR
